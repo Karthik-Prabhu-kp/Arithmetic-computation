3b68fc9c191a8bd2a82896419c04b05256602cea
Karthik-Prabhu-kp <kpwat66@gmail.com>
2020-08-28 20:12:34 +0530

    operation 3

---
+++ Arithmeticoperation3.sh
@@ -0,0 +1,12 @@
+read -p "enter 1st input" a
+read -p "enter 2nd input" b
+read -p "enter 3rd input" c
+
+operation=$(( $a+$b*$c ))
+echo "$operation"
+
+operation2=$(( $a*$b+$c ))
+echo "$operation2"
+
+operation3=$(( $c+$a/$b ))
+echo "$operation3"
